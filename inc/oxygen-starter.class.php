39c3a2a1f28effe94208f01f7583db1c18d5eba8
Simone Alati <git@simonealati.it>
2020-09-23 15:42:35 +0200

    Templating functions added

--- inc/oxygen-starter.class.php
+++ inc/oxygen-starter.class.php
@@ -68,6 +68,59 @@ class OxygenStarter {
 
     }
 
+    /**
+     * Include un template e gli passa un array di parametri
+     * @param string $slug
+     * @param string $name
+     * @param array $vars
+     * @param bool $use_cache
+     */
+    public static function getTemplatePart($slug, $name = '', $vars = null, $use_cache = false) {
+
+        if (is_array($vars) && !empty($vars)) {
+            //imposto i parametri da passare al template
+            set_query_var('oxygen_template_vars', $vars);
+        }
+
+        //compongo la chiave per la cache
+        $key = ($name == '') ? $slug : $slug . '-' . $name;
+
+        if ($use_cache === true) {
+            $template = wp_cache_get($key);
+            if ($template === false) {
+                //salvo il template in cache e lo mostro
+                ob_start();
+                get_template_part($slug, $name);
+                $template = ob_get_clean();
+                // template in cache per 5 minuti
+                wp_cache_set($key, addslashes($template), '', 300);
+                echo $template;
+            } else {
+                //mostro il template dalla cache
+                echo stripslashes($template);
+            }
+        } else {
+            //recupero il template
+            get_template_part($slug, $name);
+        }
+
+        if (is_array($vars) && !empty($vars)) {
+            // rimuovo i parametri passati
+            set_query_var('oxygen_template_vars', false);
+        }
+    }
+
+    /**
+     * Recupera le variabili passate ad un template
+     * @return array
+     */
+
+    public static function getTemplateVars() {
+        $vars = get_query_var('oxygen_template_vars');
+        if (count($vars) == 1) return $vars[0];
+        return $vars;
+    }
+
 }
 
 OxygenStarter::instance();
